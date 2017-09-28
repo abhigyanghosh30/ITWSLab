0d2a3b48b21dcbac761bb69c468dbb143f3096cb
Abhigyan Ghosh <abhigyanghosh30@gmail.com>
2017-09-28 15:31:50 +0530

    Coded it out? q2.c

--- q2.c
+++ q2.c
@@ -1,5 +1,35 @@
 #include<stdio.h>
 int main()
 {
+	int n,k,flag=0;
+	scanf("%d %d",&n,&k);
+	int a[n];
+	for(int i=0;i<n;i++)
+		scanf("%d",&a[i]);
+	for(int i=1;i<(1<<n);i++)
+	{
+		int sum=0,temp=i;
+		for(int j=0;j<n;j++,temp>>1)
+		{
+			if(temp & 1 == 1)
+			{
+				sum+=a[j];
+			}
+		}
+		if(sum==k)
+		{
+			printf("Yes");
+			flag=1;
+			temp=i;
+			for(int j=0;j<n;j++,temp>>1)
+			{
+				if(temp &1 == 1)
+					printf("%d ",a[j]);
+			}
+			break;
+		}
+	}
+	if(flag==0)
+		printf("No");
 	return 0;
 }
